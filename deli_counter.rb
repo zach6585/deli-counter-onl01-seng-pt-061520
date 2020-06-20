503e4fc3e4e249ca97eb99caaa093a5785de2855
zachary mines <zmines@wesleyan.edu>
2020-06-20 22:16:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line (array)
+  if array.size == 0 
+    puts "The line is currently empty."
+  else 
+    a = "The line is currently:" 
+    i = 1
+    while i < array.size + 1
+      a += " #{i}. #{array[i-1]}"
+      i +=1
+    end
+  puts a
+  end
+end 
+
+def take_a_number(array,name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+end 
+
+def now_serving(array)
+  if array.size == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+  end
+  array.shift
+end 
+
+
